5a1270a7ca4ed44a91006271df24ac8744de88ff
Esteban M <emolinac92@gmail.com>
2022-07-20 09:52:20 -0400

    Added torus value as a variable in reconstruction

--- reconstructed/JOB_run_gemc_fullchain_reconstruction.sh
+++ reconstructed/JOB_run_gemc_fullchain_reconstruction.sh
@@ -73,7 +73,9 @@ executable_file_check(){
 }
 
 ## DIRECTORIES
+cd ..
 main_dir=$(pwd)
+echo "main dire is ${main_dir}"
 LEPTO_dir=~/Lepto64Sim/bin ## CHECK THIS DIRECTORY!
 execution_dir=/volatile/clas12/emolinac
 lepto2dat_dir=${main_dir}/thrown/lepto2dat
@@ -87,6 +89,7 @@ out_dir_recon=/work/clas12/rg-e/emolinac/hipo_11gev_fullchain
 ## VARIABLES
 Nevents=750
 target=D
+torus=1
 id=${target}_${SLURM_ARRAY_JOB_ID}${SLURM_ARRAY_TASK_ID}
 temp_dir=${execution_dir}/${id}
 
@@ -94,6 +97,7 @@ temp_dir=${execution_dir}/${id}
 ###########################################################################
 ###########################       PREAMBLE      ###########################
 ###########################################################################
+echo "Checking directories"
 # Directory and files check
 directory_files_check
 
@@ -104,6 +108,7 @@ cd ${temp_dir}
 ###########################################################################
 ###########################       LEPTO         ###########################
 ###########################################################################
+echo "Running LEPTO"
 # Prereqs setting
 if [ -z "${CERN}" ]
 then
@@ -115,27 +120,30 @@ z_shift=0.
 
 # Copy lepto executable to temp folder
 cp ${LEPTO_dir}/lepto.exe ${temp_dir}/lepto_${id}.exe
+echo "Copying LEPTO to temp dir"
 
 # Assign the targets A and Z numbers
 AZ_assignation ${target}
 echo "${Nevents} ${A} ${Z}" > lepto_input.txt
-
+echo "Target assignation done!"
 # EXECUTE LEPTO
 lepto_${id}.exe < lepto_input.txt > ${lepto_out}.txt
-
+echo "LEPTO execution done"
 # Transform lepto's output to dat files
 cp ${lepto2dat_dir}/lepto2dat.pl ${temp_dir}/
 perl lepto2dat.pl ${z_shift} < ${lepto_out}.txt > ${lepto_out}.dat
-
+echo "lepto2dat done"
 # Transform's dat files into ROOT NTuples
 executable_file_check
+echo "dat2tuple start"
 cp ${dat2tuple_dir}/bin/dat2tuple ${temp_dir}/
 ./dat2tuple ${lepto_out}.dat ${lepto_out}_ntuple.root
-
+echo "Finished LEPTO"
 ###########################################################################
 ###########################       GEMC          ###########################
 ###########################################################################
 # Prereqs setting
+echo "Running GEMC"
 if [ -z "${GEMC_DATA_DIR}" ]
 then
     source /group/clas12/packages/setup.sh
@@ -149,14 +157,16 @@ LUND_lepto_out=LUND${lepto_out}
 cp ${leptoLUND_dir}/leptoLUND.pl ${temp_dir}/
 perl leptoLUND.pl ${z_shift} < ${lepto_out}.txt > ${LUND_lepto_out}.dat
 
-# Copy the gcard you'll use into the temp folder
+# Copy the gcard you'll use into the temp folder and set the torus value
 cp ${gcard_dir}/clas12.gcard ${temp_dir}/
+sed -i "s/TORUS_VALUE/${torus}/g" clas12.gcard
 
 # EXECUTE GEMC
 gemc clas12.gcard -INPUT_GEN_FILE="LUND, ${LUND_lepto_out}.dat" -OUTPUT="evio, ${gemc_out}.ev" -USE_GUI="0"
 echo "GEMC execution succesful!"
+
 # Transform to HIPO
-evio2hipo -t -1.0 -s -1.0 -r 11 -o ${gemc_out}.hipo -i ${gemc_out}.ev
+evio2hipo -t ${torus} -s -1.0 -r 11 -o ${gemc_out}.hipo -i ${gemc_out}.ev
 rm ${gemc_out}.ev
 echo "Evio 2 HIPO transformation successful"
 
