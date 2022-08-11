0a7b2aeda8911e610102e54abcaf4473fbd0e6b8
Esteban M <emolinac92@gmail.com>
2022-08-10 20:23:32 -0400

    gcard name bug fixed

--- reconstructed/JOB_run_full_reconstruction_fmt.sh
+++ reconstructed/JOB_run_full_reconstruction_fmt.sh
@@ -175,10 +175,10 @@ cp ${leptoLUND_dir}/leptoLUND.pl ${temp_dir}/
 perl leptoLUND.pl ${z_shift} < ${lepto_out}.txt > ${LUND_lepto_out}.dat
 
 # Copy the gcard you'll use into the temp folder and set the torus value
-cp ${gcard_dir}/clas12.gcard ${temp_dir}/
-sed -i "s/TORUS_VALUE/${torus}/g" clas12.gcard
-sed -i "s/SOLENOID_VALUE/${solenoid}/g" clas12.gcard
-sed -i "s/Z_SHIFT/${z_shift}/g" clas12.gcard
+cp ${gcard_dir}/clas12_fmt.gcard ${temp_dir}/
+sed -i "s/TORUS_VALUE/${torus}/g" clas12_fmt.gcard
+sed -i "s/SOLENOID_VALUE/${solenoid}/g" clas12_fmt.gcard
+sed -i "s/Z_SHIFT/${z_shift}/g" clas12_fmt.gcard
 
 # EXECUTE GEMC
 gemc clas12_fmt.gcard -INPUT_GEN_FILE="LUND, ${LUND_lepto_out}.dat" -OUTPUT="evio, ${gemc_out}.ev" -USE_GUI="0"
