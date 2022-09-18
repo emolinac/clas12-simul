154ba499044c3c185a5a2d20a8915aac1c943878
Esteban M <emolinac92@gmail.com>
2022-09-18 11:20:31 -0400

    change some folder iun send_jobs.sh

--- reconstructed-double-target/send_jobs.sh
+++ reconstructed-double-target/send_jobs.sh
@@ -32,8 +32,8 @@ errout_check(){
 ################################################################################################
 ############################# Hermes-like script hehe ##########################################
 ################################################################################################
-Njobs=10
-Njobsmax=5
+Njobs=100
+Njobsmax=50
 
 ################################################################################################
 ########################               Directories              ################################
@@ -46,8 +46,10 @@ lepto2dat_dir=${main_dir}/thrown/lepto2dat
 dat2tuple_dir=${main_dir}/thrown/dat2tuple
 rec_utils_dir=${main_dir}/reconstructed-double-target/utils
 
-out_dir_lepto=/work/clas12/rg-e/emolinac/lepto_11gev_fullchain
-out_dir_recon=/work/clas12/rg-e/emolinac/hipo_11gev_fullchain
+#out_dir_lepto=/work/clas12/rg-e/emolinac/lepto_11gev_fullchain
+#out_dir_recon=/work/clas12/rg-e/emolinac/hipo_11gev_fullchain
+out_dir_lepto=/volatile/clas12/emolinac/lepto_files
+out_dir_recon=/volatile/clas12/emolinac/hipo_files
 
 ################################################################################################
 ########################               Simul Specs             #################################
@@ -58,16 +60,16 @@ Nevents=750
 
 # Use    : Sets the scaling of the magnetic fields
 # Values : From -1 to 1
-torus=1
+torus=-1
 solenoid=-1
 
 # Use    : Determine ID, vertex, and set the u/d ratio in LEPTO
 # Values : D2, C, Al, Cu, Sn, Pb
-target=C
+target=D2
 
 # Use    : Determine the dt configuration present
 # Values : lD2, eg2-X, eg2-X-lD2, where X = {C,Al,Cu,Sn,Pb}
-target_variation=eg2-${target}-lD2
+target_variation=lD2
 
 # Use    : Determine the cryotarget length
 # Values : 2, 3, 5 (just the number! do not write points or things like that)
@@ -81,6 +83,6 @@ executables_check
 errout_check
 
 cd ${main_dir}/reconstructed-double-target
-sbatch --array=1-${Njobs} run_full_reconstruction_fmt_cryoresize_fullD2vertex.sh \
+sbatch --array=1-${Njobs}%${Njobsmax} run_full_reconstruction_fmt_cryoresize_fullD2vertex.sh \
 ${LEPTO_dir} ${execution_dir} ${lepto2dat_dir} ${dat2tuple_dir} ${rec_utils_dir} ${out_dir_lepto} ${out_dir_recon} \
 ${Nevents} ${torus} ${solenoid} ${target} ${target_variation} ${lD2_length}
