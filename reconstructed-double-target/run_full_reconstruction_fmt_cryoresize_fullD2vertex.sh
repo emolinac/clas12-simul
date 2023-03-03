1a664b4e7705201bd762efaa321199c16ef66223
emolinac <emolinac92@gmail.com>
2023-03-03 16:59:36 -0300

    upd: gemc output name

--- reconstructed-double-target/run_full_reconstruction_fmt_cryoresize_fullD2vertex.sh
+++ reconstructed-double-target/run_full_reconstruction_fmt_cryoresize_fullD2vertex.sh
@@ -146,7 +146,7 @@ then
     module load clas12
 fi
 
-gemc_out=gemc_out_${id}_${target_variation}_s${solenoid}_t${torus}_fmt${fmt_variation}
+gemc_out=gemc_out_${id}_${target_variation}_s${solenoid}_t${torus}_fmt${fmt_variation}_usshift${upstream_shift}
 gcard_name=clas12_fmt_cryoresize
 
 # Transform lepto's output to LUND format
